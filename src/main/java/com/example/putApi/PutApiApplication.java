6dd3963663f889abc7cb35a403d597dc2ad6d360
kpanwala <kpanwala33@gmail.com>
2022-12-31 14:31:13 +0530

    Documentation added using Open Api (Swagger)

--- src/main/java/com/example/putApi/PutApiApplication.java
+++ src/main/java/com/example/putApi/PutApiApplication.java
@@ -2,14 +2,11 @@ package com.example.putApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class PutApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/putApi/controller/Controller.java
+++ src/main/java/com/example/putApi/controller/Controller.java
@@ -1,9 +1,6 @@
 package com.example.putApi.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.putApi.Entity.User;
@@ -19,29 +17,29 @@ import com.example.putApi.service.UserService;
 @RestController
 @RequestMapping("/user")
 public class Controller {
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+	public User update(@PathVariable String id, @RequestBody User inputUsr) {
+		return this.userService.updateUser(id, inputUsr);
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public User add(@RequestBody User inputUsr) {
+		return this.userService.addUser(inputUsr);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<User> getAll() {
+		System.out.println("Inside Get All User");
+		return this.userService.getAllUsers();
+	}
 	
- @Autowired
- UserService userService;
- 
- @PutMapping("/update/{id}")
- public User update(@PathVariable String id,@RequestBody User inputUsr) {
-     return this.userService.updateUser(id, inputUsr);
- }
- 
- @PostMapping("/add")
- public User add(@RequestBody User inputUsr) {
-	 return this.userService.addUser(inputUsr);
- }
- 
- @GetMapping("")
- public List<User> getAll() {
-	 System.out.println("Inside Get All User");
-	 return this.userService.getAllUsers();
- }
- 
- @GetMapping("/{id}")
- public User getAUser(@PathVariable String id) {
-	 System.out.println("Inside Get A User");
-     return this.userService.getUser(id);
- }
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public User getAUser(@PathVariable String id) {
+		System.out.println("Inside Get A User");
+		return this.userService.getUser(id);
+	}
 }
